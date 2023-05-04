41c92a8fd6224631c173e18f359ecbd1c25a0c62
Chen <73560896+zChen-1@users.noreply.github.com>
2023-05-04 12:33:12 -0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+  // Connect to the database
+  $conn = new SQLite3('users.db');
+
+  // Get user input
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  // Insert user data into database
+  $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+  $conn->exec($sql);
+
+  // Close the database connection
+  $conn->close();
+
+  // Redirect to login page
+  header("Location: login.php");
+?>
