0ee3030101078591e941d2b703e38d9a4a78115e
Chen <73560896+zChen-1@users.noreply.github.com>
2023-05-04 20:26:08 -0700

    Update register.php

--- register.php
+++ register.php
@@ -1,25 +1,38 @@
 <?php
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  // Connect to the database
-  $conn = new SQLite3('users.db');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get user input
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-  // Get user input
-  $username = $_GET['username'];
-  $email = $_GET['email'];
-  $password = $_GET['password'];
+    // Hash the password for security
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-  // Hash the password for security
-  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    // Connect to the database
+    $db_user = 'root';
+    $db_pass = '123456';
+    $db_name = 'users';
+    $cloud_sql_connection_name = 'hybrid-zephyr-362606:us-central1:cpsc491project';
+    $mysqli = new mysqli(null, $db_user, $db_pass, $db_name, null, "/cloudsql/$cloud_sql_connection_name");
 
-  // Insert the user data into the "users" table
-  $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
-  $result = $conn->query($sql);
+    if ($mysqli->connect_errno) {
+        die('Failed to connect to MySQL: ' . $mysqli->connect_error);
+    }
 
-  // Close the database connection
-  $conn->close();
+    // Insert the user data into the "users" table
+    $stmt = $mysqli->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $email, $hashed_password);
+    $result = $stmt->execute();
 
-  // Redirect the user to index.html
-  header('Location: index.html');
-  exit;
+    if ($result === false) {
+        die('Error inserting data into the database: ' . $mysqli->error);
+    }
+
+    // Close the database connection
+    $mysqli->close();
+
+    // Redirect the user to index.html
+    header('Location: index.html');
+    exit;
 }
 ?>
