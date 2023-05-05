04f3f15a5afd57c8575558ab904d299d9d30dbed
Chen <73560896+zChen-1@users.noreply.github.com>
2023-05-04 20:35:22 -0700

    Update register.php

--- register.php
+++ register.php
@@ -8,28 +8,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Hash the password for security
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    // Connect to the database
+    // Connect to Cloud SQL instance
+    $db_host = 'hybrid-zephyr-362606:us-central1:cpsc491project';
     $db_user = 'root';
     $db_pass = '123456';
     $db_name = 'users';
-    $cloud_sql_connection_name = 'hybrid-zephyr-362606:us-central1:cpsc491project';
-    $mysqli = new mysqli(null, $db_user, $db_pass, $db_name, null, "/cloudsql/$cloud_sql_connection_name");
 
-    if ($mysqli->connect_errno) {
-        die('Failed to connect to MySQL: ' . $mysqli->connect_error);
+    $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
+
+    try {
+        $pdo = new PDO($dsn, $db_user, $db_pass);
+    } catch (PDOException $e) {
+        die('Connection failed: ' . $e->getMessage());
     }
 
     // Insert the user data into the "users" table
-    $stmt = $mysqli->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $username, $email, $hashed_password);
+    $sql = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+    $stmt->bindValue(':password', $hashed_password, PDO::PARAM_STR);
     $result = $stmt->execute();
 
     if ($result === false) {
-        die('Error inserting data into the database: ' . $mysqli->error);
+        die('Error inserting data into the database: ' . $pdo->errorInfo()[2]);
     }
 
     // Close the database connection
-    $mysqli->close();
+    $pdo = null;
 
     // Redirect the user to index.html
     header('Location: index.html');
