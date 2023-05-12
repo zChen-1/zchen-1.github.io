6ab3ba5e529dde515be362891b994ecd6ce94668
Chen <czh2191@live.com>
2023-05-11 19:09:43 -0700

    1

--- profile.js
+++ profile.js
@@ -1,21 +1,17 @@
 window.onload = function() {
-    // Get the username and email from local storage
     var username = localStorage.getItem('username');
     var email = localStorage.getItem('email');
 
     // Check if the username and email are not null
     if (username && email) {
-        // Update the welcome message
         document.getElementById('welcomeMessage').innerText = "Welcome, " + username;
-
-        // Update the username and email paragraphs
         document.getElementById('username').innerText = "Username: " + username;
         document.getElementById('email').innerText = "Email: " + email;
     } else {
         // If username or email are null, display a message and redirect to the registration page
         alert('Please register first!');
         setTimeout(function() {
-            window.location.href = "register.html";  // Modify this to match your registration page URL
+            window.location.href = "register.html";
         }, 2000);
     }
 }
